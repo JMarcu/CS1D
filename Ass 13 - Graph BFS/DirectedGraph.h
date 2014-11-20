93dee4bdde27a4c65bf4c04a2120a1a1eb05cef0
James Marcu <james.marcu@yahoo.com>
2014-11-19 23:19:57 -0800

    Some comments added.

--- Ass 13 - Graph BFS/DirectedGraph.h
+++ Ass 13 - Graph BFS/DirectedGraph.h
@@ -14,84 +14,151 @@
 #include <iostream>
 using namespace std;
 
+//A vertex for a graph.
 template <typename ElemType>
 class Vertex
 {
 	public:
-		Vertex()
+	   /*******************************************
+	    * * * * CONSTRUCTORS AND DESTRUCTOR * * * *
+	    *******************************************/
+		Vertex()                                     //Default
 			{position = -1;}
-		Vertex(const ElemType& elemInit, int posInit)
-			{element = elemInit; position = posInit;}
+		Vertex(const ElemType& elemInit, int posInit)//Initializes the
+			{element = elemInit; position = posInit;}//data and position.
 
-		const ElemType& GetElem() const
+	   /*******************************************
+		* * * *          ACCESSORS          * * * *
+		*******************************************/
+		const ElemType& GetElem() const//Returns the vertex's element.
 			{return element;}
-		int       GetPosition() const
+		int GetPosition() const        //Returns the position of the vertex.
 			{return position;}
+		//These operators are overloaded to compare the verteces' elements.
 		bool operator==(const Vertex<ElemType>& compareTo) const
 			{return element != compareTo.GetElem();}
 		bool operator!=(const Vertex<ElemType>& compareTo) const
 			{return element != compareTo.GetElem();}
 
 	private:
-		ElemType element;
-		int      position;
+		ElemType element; //The data stored in the vertex.
+		int      position;//The position of the vertex, used in the
+		                  //adjacenecy matrix.
 };
 
+//A directed edge for a graph.
 template <typename ElemType>
 class DirectedEdge
 {
 	public:
-		DirectedEdge()
+	   /*******************************************
+	    * * * * CONSTRUCTORS AND DESTRUCTOR * * * *
+	    *******************************************/
+		DirectedEdge()                                      //Default constructor
 			{origin = NULL; destination = NULL; weight = 0;}
-		DirectedEdge(const Vertex<ElemType>* originInit, const Vertex<ElemType>* destInit, int weightInit)
+		DirectedEdge(const Vertex<ElemType>* originInit,    //Initializes
+				     const Vertex<ElemType>* destInit,      //values of edge.
+				     int weightInit)
 			{origin = originInit; destination = destInit; weight = weightInit;}
 
-		const Vertex<ElemType>* GetOrigin() const
+	   /*******************************************
+		* * * *          ACCESSORS          * * * *
+		*******************************************/
+		const Vertex<ElemType>* GetOrigin() const     //Returns edge's origin.
 			{return origin;}
-		const Vertex<ElemType>* GetDestination() const
+		const Vertex<ElemType>* GetDestination() const//Returns the destination.
 			{return destination;}
-		int GetWeight() const
+		int GetWeight() const                         //Returns edge's weight.
 			{return weight;}
 
 	private:
-		const Vertex<ElemType>* origin;
-		const Vertex<ElemType>* destination;
-		int weight;
+		const Vertex<ElemType>* origin;     //Where the edge is coming from.
+		const Vertex<ElemType>* destination;//Where the edge is going to.
+		int weight;                         //Weight of the edge.
 };
 
 template <typename ElemType>
 class DirectedGraph
 {
 	public:
-		DirectedGraph();
+	   /*******************************************
+	    * * * * CONSTRUCTORS AND DESTRUCTOR * * * *
+	    *******************************************/
+		DirectedGraph();//Default
 
+
+	   /*******************************************
+		* * * *          ACCESSORS          * * * *
+		*******************************************/
+		//Returns a pointer to a vertex with the provided search key.
 		const Vertex<ElemType>* GetVertex(const ElemType& key);
+
+		//A depth first search.
 		void DFS(const Vertex<ElemType>* start,
                  List<DirectedEdge<ElemType>* >& discoveryEdges,
                  List<DirectedEdge<ElemType>* >& backEdges,
                  List<DirectedEdge<ElemType>* >& crossEdges,
                  List<DirectedEdge<ElemType>* >& forwardEdges,
                  bool& isStronglyConnected = false) const;
-		bool IsStronglyConnected(const Vertex<ElemType>* start) const;
-		void BFS(const Vertex<ElemType>* start) const;
-		void PrintBFS(const List<List<const Vertex<ElemType>* > >& toPrint) const;
-		void PrintPositions() const;
-		void PrintMatrix() const;
 
+		//Returns true if the graph is strongly connected.
+		bool IsStronglyConnected(const Vertex<ElemType>* start) const;
+
+		//A breadth first search.
+		void BFS(const Vertex<ElemType>* start) const;
+
+
+	   /*******************************************
+		* * * *          MUTATORS           * * * *
+		*******************************************/
+		//Adds a vertex to the graph.
 		void AddVertex(const ElemType& elemInit);
-		void AddEdge(const Vertex<ElemType>* origin, const Vertex<ElemType>* destination, int weight);
+
+		//Adds an edge to the graph.
+		void AddEdge(const Vertex<ElemType>* origin,
+				     const Vertex<ElemType>* destination,
+				     int weight);
+
+		//Erases a vertex from the graph.
 		void EraseVertex(Vertex<ElemType>* toErase);
+
+		//Erases an edge from the graph.
 		void EraseEdge(DirectedEdge<ElemType>* toErase);
 
 	private:
-		List<Vertex<ElemType> > vertexList;
-		List<DirectedEdge<ElemType> > edgeList;
-		DirectedEdge<ElemType>*** adjacencyMatrix;
-		int  size;
+	   /*******************************************
+		* * * *         VARIABLES           * * * *
+		*******************************************/
+		List<Vertex<ElemType> > vertexList;       //List of verteces.
+		List<DirectedEdge<ElemType> > edgeList;   //List of edges.
+		DirectedEdge<ElemType>*** adjacencyMatrix;//Adjacency matrix.
+		int  size;                                //Number of verteces.
 
+	   /*******************************************
+		* * * *          ACCESSORS          * * * *
+		*******************************************/
+		//Prints the results of a BFS to the console.
+		void PrintBFS(const List<List<const Vertex<ElemType>* > >& toPrint) const;
+
+	   /*******************************************
+		* * * *          MUTATORS           * * * *
+		*******************************************/
+		//Increases the size of the adjacency matrix by one.
 		void IncrementMatrix();
+
 };
 
+/*************************************************************************
+ * METHOD Constructor
+ * ______________________________________________________________________
+ * Default constructor.
+ * ______________________________________________________________________
+ * PRE-CONDITIONS -
+ *  <none>
+ *
+ * POST-CONDITIONS -
+ * 	Instantiates a directed graph.
+ *************************************************************************/
 template <typename ElemType>
 DirectedGraph<ElemType>::DirectedGraph()
 {
@@ -101,21 +168,43 @@ DirectedGraph<ElemType>::DirectedGraph()
 	adjacencyMatrix[0] = new DirectedEdge<ElemType>* [0];
 }
 
+/*************************************************************************
+ * METHOD GetVertex
+ * ______________________________________________________________________
+ * Returns a pointer to a vertex in directed graph whose element matches
+ * the provided search key.
+ * ______________________________________________________________________
+ * PRE-CONDITIONS -
+ *  key : The element that you are searching the graph for.
+ *
+ * POST-CONDITIONS -
+ * 	Returns a pointer to the matching vertex, or NULL if not found.
+ *************************************************************************/
 template <typename ElemType>
 const Vertex<ElemType>* DirectedGraph<ElemType>::GetVertex(const ElemType& key)
 {
+	//VARIABLE DECLERATIONS
+	//PROC & OUT - Searches the vertex list.
 	typename List<Vertex<ElemType> >::Iterator searchIt;
-	bool found;
-	bool endOfSearch;
+	bool found;      //PROC - If the vertex is found yet.
+	bool endOfSearch;//PROC - If all verteces have been searched.
 
+	//VARIABLE INITIALIZATIONS
 	searchIt    = vertexList.Begin();
 	found       = false;
 	endOfSearch = false;
 
+	/*********************************************************************
+	 * PROC - Searches until either a matching vertex is found or all
+	 *        verteces have been searched.
+	 *********************************************************************/
 	while (!found && !endOfSearch)
 	{
+		//First checks if we're at the end of the vertex list. If we are
+		//then end the search. Otherwise check the current vertex.
 		if(searchIt != vertexList.End())
 		{
+			//If it matches the key then set found to true.
 			if(searchIt->GetElem().GetElem() == key)
 			{
 				found = true;
@@ -131,6 +220,8 @@ const Vertex<ElemType>* DirectedGraph<ElemType>::GetVertex(const ElemType& key)
 		}
 	}
 
+	//If the key was found then return a pointer to it. Otherwise return
+	//NULL.
 	if(found)
 	{
 		return &(*searchIt);
@@ -141,6 +232,29 @@ const Vertex<ElemType>* DirectedGraph<ElemType>::GetVertex(const ElemType& key)
 	}
 };
 
+/*************************************************************************
+ * METHOD DFS
+ * ______________________________________________________________________
+ * Performs a depth first search on the graph and returns all relevant
+ * information.
+ * ______________________________________________________________________
+ * PRE-CONDITIONS -
+ *  start               : Points to the first vertex in the search.
+ *  discoveryEdges      : A list for storing edges.
+ *  backEdges           : A list for storing edges.
+ *  crossEdges          : A list for storing edges.
+ *  forwardEdges        : A list for storing edges.
+ *  isStronglyConnected : Will store if the graph is strongly connected.
+ *                        If this field is not provided the check will not
+ *                        be done.
+ *
+ * POST-CONDITIONS -
+ *  discoveryEdges      : All discovery edges will have been added to the list.
+ *  backEdges           : All back edges will have been added to the list.
+ *  crossEdges          : All cross edges will have been added to the list.
+ *  forwardEdges        : All forward edges will have been added to the list.
+ *  isStronglyConnected : If the graph is strongly connected.
+ *************************************************************************/
 template <typename ElemType>
 void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
         						  List<DirectedEdge<ElemType>* >& discoveryEdges,
@@ -149,21 +263,55 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
         						  List<DirectedEdge<ElemType>* >& forwardEdges,
         						  bool& isStronglyConnected) const
 {
+	/*********************************************************************
+	 * VARIABLE DECLERATIONS
+	 *********************************************************************/
+	//PROC - List of all unvisited nodes.
 	List<const Vertex<ElemType>* >  unvisited;
-	List<const Vertex<ElemType>* >  active;
-	List<DirectedEdge<ElemType>* >  visitedEdges;
-	typename List<DirectedEdge<ElemType>* >::Iterator edgeIt;
-	Stack<const Vertex<ElemType>* > history;
-	const Vertex<ElemType>*   currentVertex;
-	const Vertex<ElemType>*   nextVertex;
-	DirectedEdge<ElemType>*   edgePtr;
-	bool                      searchForVertex;
-	bool                      deadEnd;
-	int                       index;
-	int                       edgeTimeCount;
-	int                       edgeTimeOne;
-	int                       edgeTimeTwo;
 
+	//PROC - List of all 'active' nodes. Nodes are active if they or their
+	//       ancestors are being visited.
+	List<const Vertex<ElemType>* >  active;
+
+	//PROC - List of all edges which have been looked at.
+	List<DirectedEdge<ElemType>* >  visitedEdges;
+
+	//PROC - Used to traverse lists of edges.
+	typename List<DirectedEdge<ElemType>* >::Iterator edgeIt;
+
+	//PROC - A stack of visited nodes. Used to backtrack.
+	Stack<const Vertex<ElemType>* > history;
+
+	//PROC - Points to the vertex currently being visited.
+	const Vertex<ElemType>*   currentVertex;
+
+	//PROC - Points to the vertex which will be visited next.
+	const Vertex<ElemType>*   nextVertex;
+
+	//PROC - Used to point at edges for inspection.
+	DirectedEdge<ElemType>*   edgePtr;
+
+	//PROC - Whether all connected vertex's of the current vertex have been
+	//       checked yet.
+	bool searchForVertex;
+
+	//PROC - Whether the current vertex was a dead end.
+	bool deadEnd;
+
+	//PROC - Used to access the adjacency matrix.
+	int  index;
+
+	//PROC - Stores an edge's position in visitedEdges.
+	int  edgeTimeCount;
+
+	//PROC - These keep track of the position of two edges in visitedEdges.
+	int  edgeTimeOne;
+	int  edgeTimeTwo;
+
+	/*********************************************************************
+	 * PROC - Instantiates unvisited, a list of pointers to all unvisited
+	 *        verteces.
+	 *********************************************************************/
 	typename List<Vertex<ElemType> >::Iterator vertexIt;
 	vertexIt = vertexList.Begin();
 	const Vertex<ElemType>** vertexAuxPtr;
@@ -174,52 +322,78 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
 		++vertexIt;
 	}
 
+	/*********************************************************************
+	 * PROC - Sets up the initial conditions of the search.
+	 *********************************************************************/
 	currentVertex = start;
 	active.InsertBack(start);
-
 	history.Push(start);
 	unvisited.Erase(start);
 
+	/*********************************************************************
+	 * PROC - The search continues until history is empty. This indicates
+	 *        all connected verteces have been visited.
+	 *********************************************************************/
 	while (!history.Empty())
 	{
+		//Reset variables at the top of the loop.
 		searchForVertex = true;
 		deadEnd         = false;
 		index           = 0;
 
+		//Will search for an unvisited vertex connected to the current
+		//vertex.
 		while (searchForVertex)
 		{
+			//If we have checked every index of the adjacency matrix then
+			//this is a dead end.
 			if (index >= size)
 			{
 				searchForVertex = false;
 				deadEnd         = true;
 			}
+			//If there is an edge at this index...
 			else if(adjacencyMatrix[currentVertex->GetPosition()][index] != NULL)
 			{
+				//Set edgePtr to point at the edge and nextVertex to point
+				//at its destination.
 				edgePtr = adjacencyMatrix[currentVertex->GetPosition()][index];
 				nextVertex = edgePtr->GetDestination();
 
+				//If the destination has not been visited yet...
 				if (unvisited.Search(nextVertex) != unvisited.End())
 				{
+					//The edge is a discvoery edge.
 					discoveryEdges.InsertBack(edgePtr);
 					visitedEdges.InsertBack(edgePtr);
 
+					//Add the next vertex to history, remove it from
+					//unvisited, and add it to active.
 					history.Push(nextVertex);
 					unvisited.Erase(nextVertex);
 					active.InsertBack(nextVertex);
 
+					//Update current vertex to point at the new vertex.
 					currentVertex = nextVertex;
 
+					//We're done searching this vertex for now.
 					searchForVertex = false;
 				}
+				//If the vertex had been visited, but we haven't tried this
+				//edge before...
 				else if (visitedEdges.Search(edgePtr) == visitedEdges.End())
 				{
+					//If the destination of the edge is not active then
+					//it is either a cross or forward edge.
 					if(active.Search(edgePtr->GetDestination()) == active.End())
 					{
+						//Reset variables.
 						edgeTimeCount = 0;
 						edgeTimeOne   = visitedEdges.Size() + 1;
 						edgeTimeTwo   = -1;
 						edgeIt = visitedEdges.Begin();
 
+						//Discover when the destination was visited.
 						while(edgeTimeTwo < 0)
 						{
 							if((*edgeIt)->GetDestination() == edgePtr->GetDestination())
@@ -233,6 +407,9 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
 							}
 						}
 
+						//If the destination was visited before the current
+						//node it is a cross edge. Otherwise it is a
+						//forward edge.
 						if (edgeTimeOne < edgeTimeTwo)
 						{
 							crossEdges.InsertBack(edgePtr);
@@ -244,6 +421,8 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
 							visitedEdges.InsertBack(edgePtr);
 						}
 					}
+					//If, however, the destination vertex is still active
+					//then this is a back edge.
 					else
 					{
 						backEdges.InsertBack(edgePtr);
@@ -253,15 +432,20 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
 				}
 			}
 
+			//Check the next index in vertex's adjacency matrix.
 			++index;
 		}
 
+		//If the last vertex was a dead end...
 		if(deadEnd)
 		{
+			//The current vertex is no longer active.
 			active.Erase(history.Peek());
 
+			//Remove it from history.
 			history.Pop();
 
+			//The current vertex is now the old one's parent.
 			if(!history.Empty())
 			{
 				currentVertex = history.Peek();
@@ -273,8 +457,12 @@ void DirectedGraph<ElemType>::DFS(const Vertex<ElemType>* start,
 		}
 	}
 
+	//If the user provided a variable for isStrongly connected then run
+	//a check for that.
 	if (isStronglyConnected)
 	{
+		//If unvisited is not empty then the graph is not strongly connected.
+		//If it is, we have to check again with all edges reversed.
 		if(unvisited.Empty())
 		{
 			isStronglyConnected = IsStronglyConnected(start);
@@ -540,41 +728,6 @@ void DirectedGraph<ElemType>::AddEdge(const Vertex<ElemType>* origin, const Vert
 	edgeList.InsertBack(*addPtr);
 
 	adjacencyMatrix[origin->GetPosition()][destination->GetPosition()] = addPtr;
-}
-
-template <typename ElemType>
-void DirectedGraph<ElemType>::PrintPositions() const
-{
-	typename List<Vertex<ElemType> >::Iterator it;
-
-	it = vertexList.Begin();
-
-	while(it != vertexList.End())
-	{
-		cout << (*it).GetElem() << ": " << (*it).GetPosition() << endl;
-		++it;
-	}
-}
-
-template <typename ElemType>
-void DirectedGraph<ElemType>::PrintMatrix() const
-{
-	for (int i = 0; i < size; ++i)
-	{
-		for(int j = 0; j < size; ++j)
-		{
-			if(adjacencyMatrix[i][j] != NULL)
-			{
-				cout << adjacencyMatrix[i][j]->GetDestination()->GetElem();
-			}
-			else
-			{
-				cout << "NULL";
-			}
-			cout << "  ";
-		}
-		cout << endl;
-	}
 }
 
 #endif /* DIRECTEDGRAPH_H_ */
--- Ass 13 - Graph BFS/main.c++
+++ Ass 13 - Graph BFS/main.c++
@@ -1,24 +1,39 @@
 /*************************************************************************
  * AUTHOR      : James Marcu
  * STUDENT ID  : 374443
- * ASS #      : 
+ * ASS #13     : Directed Graph
  * CLASS       : CS1D
  * SECTION     : TTh 3:30 AM
- * DUE DATE    : 
+ * DUE DATE    : 11/20/2014
  *************************************************************************/
 
 #include "header.h"
 
+/*************************************************************************
+ * Directed Graph - Assignment 13
+ * -----------------------------------------------------------------------
+ * This program tests the implementation of a directed graph and its
+ * depth-first search and breadth-first search functions.
+ * -----------------------------------------------------------------------
+ * INPUT -
+ * 	Input is hardcoded within the InstantiateGraph function.
+ *
+ * OUTPUT -
+ * 	Outputs the results of the DFS and BFS functions.
+ *************************************************************************/
 int main()
 {
-	DirectedGraph<string>        graph;
-	List<DirectedEdge<string>* > discoveryEdges;
-	List<DirectedEdge<string>* > backEdges;
-	List<DirectedEdge<string>* > crossEdges;
-	List<DirectedEdge<string>* > forwardEdges;
-	bool                         graphIsConnected;
-	typename List<DirectedEdge<string>* >::Iterator edgeIt;
+	//VARIABLE DECLERATIONS
+	DirectedGraph<string>        graph;           //The graph.
+	List<DirectedEdge<string>* > discoveryEdges;  //Discovery edges from the DFS.
+	List<DirectedEdge<string>* > backEdges;       //Back edges from the DFS.
+	List<DirectedEdge<string>* > crossEdges;      //Cross edges from the DFS.
+	List<DirectedEdge<string>* > forwardEdges;    //Forward edges from the DFS.
+	bool                         graphIsConnected;//If the graph is strongly connected.
+	typename List<DirectedEdge<string>* >::Iterator edgeIt;//Used to iterate
+	                                                       //lists of edges.
 
+	//VARIABLE INITIALIZATIONS
 	graphIsConnected = true;
 
 	/*********************************************************************
@@ -27,8 +42,15 @@ int main()
 	ClassHeader("James Marcu", "374443", "CS1D", "TTh 3:30 PM",
 			    'A', 13, "Directed Graphs", cout);
 
+	/*********************************************************************
+	 * IN - Initializes the graph using hardcoded data.
+	 *********************************************************************/
 	InstantiateGraph(graph);
 
+	/*********************************************************************
+	 * PROC - Runs the DFS search on the graph starting at Seattle. The
+	 *        edges will be returned in the provided lists.
+	 *********************************************************************/
 	cout << "Now conducting a depth first search...\n";
 	graph.DFS(graph.GetVertex("Seattle"),
 			  discoveryEdges,
@@ -37,6 +59,9 @@ int main()
 			  forwardEdges,
 			  graphIsConnected);
 
+	/*********************************************************************
+	 * OUT - Outputs if the graph was strongly connected.
+	 *********************************************************************/
 	if(graphIsConnected)
 	{
 		cout << "The graph is strongly connected.";
@@ -47,6 +72,9 @@ int main()
 	}
 	cout << endl << endl;
 
+	/*********************************************************************
+	 * OUT - Outputs the discovery edges from the DFS.
+	 *********************************************************************/
 	cout << "The graphs discovery edges are: \n";
 	edgeIt = discoveryEdges.Begin();
 	while(edgeIt != discoveryEdges.End())
@@ -64,6 +92,9 @@ int main()
 	}
 	cout << endl;
 
+	/*********************************************************************
+	 * OUT - Outputs the back edges from the DFS.
+	 *********************************************************************/
 	cout << "The graphs back edges are: \n";
 	edgeIt = backEdges.Begin();
 	while(edgeIt != backEdges.End())
@@ -81,6 +112,9 @@ int main()
 	}
 	cout << endl;
 
+	/*********************************************************************
+	 * OUT - Outputs the forward edges from the DFS.
+	 *********************************************************************/
 	cout << "The graphs forward edges are: \n";
 	edgeIt = forwardEdges.Begin();
 	if (forwardEdges.Size() > 0)
@@ -105,7 +139,9 @@ int main()
 		cout << "None.\n\n";
 	}
 
-
+	/*********************************************************************
+	 * OUT - Outputs the cross edges from the DFS.
+	 *********************************************************************/
 	cout << "The graphs cross edges are: \n";
 	edgeIt = crossEdges.Begin();
 	while(edgeIt != crossEdges.End())
@@ -122,6 +158,11 @@ int main()
 	}
 	cout << endl << endl;
 
+	/*********************************************************************
+	 * PROC & OUT - Runs a breadth first search on the graph starting at
+	 *              Boston. This function will print the results to the
+	 *              console internally.
+	 *********************************************************************/
 	cout << "Now conducting a breadth first search...\n";
 	graph.BFS(graph.GetVertex("Boston"));
 
